2b99889d21e056bf0610c63ab01692dba8ff286a
waynewhying <wayne500000w@gmail.com>
2022-05-20 12:01:52 +0800

    add code 0075 & the most efficient

---
+++ 0001-0100/0075_Sort Colors.py
@@ -0,0 +1,22 @@
+class Solution:
+    def sortColors(self, nums: list[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        c_count = [0, 0, 0]
+        for num in nums:
+            if num == 0:
+                c_count[0] += 1
+            elif num == 1:
+                c_count[1] += 1
+            else:
+                c_count[2] += 1
+        
+        count = 0
+        for i in range(3):
+            while True:
+                if c_count[i] == 0:
+                    break
+                nums[count] = i
+                c_count[i] -= 1
+                count += 1
